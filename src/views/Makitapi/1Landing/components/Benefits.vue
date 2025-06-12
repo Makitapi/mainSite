789875f34bcac23357238602b694bb29b4649219
Tiago Lagarto <info@makitapi.com>
2025-06-12 08:55:58 +0100

    removed unused files, small text corrections | 0.994

--- src/views/Makitapi/1Landing/components/Benefits.vue
+++ src/views/Makitapi/1Landing/components/Benefits.vue
@@ -1,6 +1,6 @@
 <template>
   <b-container class="py-5">
-    <h2 class="h1 text-center">Benefits</h2>
+    <h2 class="h1 text-center">RFID Benefits</h2>
     <p class="text-center pb-4 mb-2 mb-lg-3">Major outcomes of a well implemented RFID program</p>
     <b-row class="row-cols-1 row-cols-sm-2 row-cols-lg-3 g-4">
       <b-col v-for="(Benefits, idx) in Benefits" :key="idx">
--- src/views/Makitapi/1Landing/components/WhyMakitapi.vue
+++ src/views/Makitapi/1Landing/components/WhyMakitapi.vue
@@ -1,6 +1,6 @@
 <template>
   <b-container class="py-5">
-    <h2 class="h1 text-center">Why Makitapi</h2>
+    <h2 class="h1 text-center">What makes us different</h2>
     <p class="text-center pb-4 mb-2 mb-lg-3">What to expect when working with our solutions</p>
     <b-row class="row-cols-1 row-cols-sm-2 row-cols-lg-3 g-4">
       <b-col v-for="(WhyMakitapi, idx) in WhyMakitapi" :key="idx">
