4af64bb71605f1590a4fa49555f943596ad71b16
Tiago Lagarto <info@makitapi.com>
2025-06-13 14:51:27 +0100

    update use-case imgs | 0.9994

--- src/views/Makitapi/2UseCases/components/Hero.vue
+++ src/views/Makitapi/2UseCases/components/Hero.vue
@@ -1,12 +1,8 @@
 <template>
   <b-container fluid class="px-sm-4 px-md-5">
-    <div class="bg-dark rounded-5 position-relative overflow-hidden px-sm-2 px-xl-3 mt-1 mx-md-n3 mx-lg-auto" style="max-width: 1660px" data-bs-theme="dark">
+    <div class="bg-dark rounded-5 position-relative overflow-hidden mx-md-n3 mx-lg-auto" style="max-width: 1660px" data-bs-theme="dark">
       <Jarallax
         custom-class="jarallax position-absolute w-100 h-100"
-        :custom-style="{
-          width: 'calc(100% + 60px)',
-          height: 'calc(100% + 60px)'
-        }"
         :options="{
           speed: 0.6,
           imgSrc: heroWave,
@@ -23,7 +19,7 @@
             <h1 class="display-5 text-center text-lg-start mb-xxl-5">Common use cases<br /><span class="text-info"> for supply chain and manufactoring</span></h1>
           </b-col>
           <b-col lg="5" xl="4" class="offset-lg-1 offset-xxl-2 d-flex flex-column pt-2 pt-md-4">
-            <p class="text-body fs-xl text-center text-lg-start pb-2 pb-md-0 mb-4 mb-md-5">There are endless ways to capitalize on RFID solutions. Here's snapshot of the most common ones.</p>
+            <p class="text-body fs-xl text-center text-lg-start pb-2 pb-md-0 mb-4 mb-md-5">There are endless ways to capitalize on RFID solutions. Here's a few examples.</p>
             <div class="d-flex flex-column flex-sm-row align-items-center justify-content-center justify-content-lg-end pb-3 pb-sm-5">
               <a class="btn btn-lg btn-primary rounded-pill w-100 w-sm-auto me-sm-3 me-xl-4 mb-2 mb-sm-0" href="/contact">Contact us</a>
               <!-- <div class="btn btn-lg btn-link text-white w-100 w-sm-auto px-2">
