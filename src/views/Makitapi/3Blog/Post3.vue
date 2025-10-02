1aeb98d01bbd79ec15c5f8457aaa4f6c3a1a905e
Tiago Lagarto <info@makitapi.com>
2025-10-02 14:45:22 +0100

    blog post 4 | 1.04

--- src/views/Makitapi/3Blog/Post3.vue
+++ src/views/Makitapi/3Blog/Post3.vue
@@ -120,7 +120,7 @@
 import NavBar from '@/components/NavBar/NavBar.vue'
 
 import Jarallax from '@/components/Jarallax.vue'
-import coverImg from '@/assets/img/blog/post3.jpeg'
+import coverImg from '@/assets/img/blog/Post3.jpeg'
 import Image1 from '@/assets/img/blog/data.jpg'
 
 import Footer from '@/views/Makitapi/1Landing/components/Footer.vue'
